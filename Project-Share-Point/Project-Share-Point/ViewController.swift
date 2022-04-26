567438aaa767c7a6219eed0150e26194db509505
Vesile <vesile_emre@hotmail.com>
2022-04-26 17:15:50 +0300

    Swift file is created.

---
+++ Project-Share-Point/Project-Share-Point/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-Share-Point
+//
+//  Created by vesile çelik on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
